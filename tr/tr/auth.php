73171e8b76f061bcdc2d93f3e0d9418540f648d4
Sang Nguyen <sangnguyenplus@gmail.com>
2024-05-20 10:44:48 +0700

    Update translations for PT

--- tr/tr/auth.php
+++ tr/tr/auth.php
@@ -1,9 +1,18 @@
 <?php
 
-declare(strict_types=1);
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
 
 return [
     'failed'   => 'Bu kimlik bilgileri kayıtlarımızla eşleşmiyor.',
-    'password' => 'Parola geçersiz.',
+    'password' => 'Girilen parola geçersiz.',
     'throttle' => 'Çok fazla giriş denemesi. :seconds saniye sonra lütfen tekrar deneyin.',
 ];
